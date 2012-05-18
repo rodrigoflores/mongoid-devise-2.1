0b21d9c9917a0f0ef8459d52488e9488c57f0d7f
Rodrigo Flores <rodrigo.flores@plataformatec.com.br>
2012-05-17 22:10:44 -0300

    Added example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoidDevise21::Application.config.secret_token = 'c3d0c5727e904d0738da80c244a3abaea682e47e29ab1c19d4806916ac1a3eebedfa0027f6b77c6d2adfe8abd88b36a33e525ba419f3626fd4751751148be55f'
